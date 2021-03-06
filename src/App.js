e7f1a892b2265e641ae025853719dbdcccd77b3b
MironenkoNadiia <candleflame@i.ua>
2021-03-06 16:42:14 +0200

    last fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import "./App.css";
-import HomePage from "./views/HomePage";
-import MoviesPage from "./views/HomePage";
-import MovieDetailsPage from "./views/MovieDetailsPage";
-import Cast from "./views/Cast";
-import Reviews from "./views/Reviews";
+import FilmSearch from "./components/FilmSearch";
 
 const App = () => (
   <>
-    <Route path={"/"} component={HomePage}></Route>
-    <Route path={"/movies"} component={MoviesPage}></Route>
-    <Route path={"/movies/:movieId"} component={MovieDetailsPage}></Route>
-    <Route path={"/movies/:movieId/cast"} component={Cast}></Route>
-    <Route path={"/movies/:movieId/reviews"} component={Reviews}></Route>
+    <FilmSearch />
   </>
 );
 
